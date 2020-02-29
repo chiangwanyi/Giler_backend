2b28cdd82a4e7cb90194184ec4d51107c86c90b4
chiangwanyi <chiangwanyi@163.com>
2020-02-29 15:57:21 +0800

    bug

--- api/common.go
+++ api/common.go
@@ -1,1 +1,44 @@
 package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"giler-backend/serializer"
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+// GetUserOpenID 获取用户 OpenID
+func GetUserOpenID(c *gin.Context) {
+	client := &http.Client{Timeout: 5 * time.Second}
+	code := c.Query("code")
+	resp, err := client.Get("https://api.weixin.qq.com/sns/jscode2session?appid=" + os.Getenv("WX_APPID") + "&secret=" + os.Getenv("WX_SECRET") + "&js_code=" + code + "&grant_type=authorization_code")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, serializer.InternalServerErrorResponse(err, "无法访问外部服务器"))
+	}
+	defer resp.Body.Close()
+	var buffer [512]byte
+	result := bytes.NewBuffer(nil)
+	for {
+		n, err := resp.Body.Read(buffer[0:])
+		result.Write(buffer[0:n])
+		if err != nil && err == io.EOF {
+			break
+		} else if err != nil {
+			c.JSON(http.StatusInternalServerError, serializer.InternalServerErrorResponse(err, "转码失败"))
+		}
+	}
+
+	param := make(map[string]interface{})
+	_ = json.NewDecoder(result).Decode(&param)
+
+	c.JSON(http.StatusOK, serializer.Response{
+		Status: serializer.OK,
+		Msg:    "访问外部服务器成功",
+		Data:   param,
+		Error:  nil,
+	})
+}
--- server/router.go
+++ server/router.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"giler-backend/api"
 	"giler-backend/middlewares"
 	"github.com/gin-gonic/gin"
 )
@@ -9,6 +10,15 @@ func NewRouter() *gin.Engine {
 	r := gin.Default()
 
 	r.Use(middlewares.Cors())
+
+	user := r.Group("/api/v1")
+	{
+		user.POST("user", api.CreateUser)
+		user.GET("user", api.QueryUser)
+		user.GET("users", api.ListUser)
+		user.PUT("user/:id", api.UpdateUser)
+		user.DELETE("user/:id", api.DeleteUser)
+	}
 
 	return r
 }
